479e602cd03975a89d311c866f0b6ac491cb6976
Robert Brauer <robert.brauer1988@googlemail.com>
2019-07-22 16:12:09 +0200

    planet position variation.

--- tools/polaris_tools_custom/source.py
+++ tools/polaris_tools_custom/source.py
@@ -186,10 +186,14 @@ class GGTauStars(StellarSource):
         self.angle_Ab = self.angle_Aa + np.pi
 
         # Cite: position of planet (Dutrey et al. 2014)
-        # self.a_planet = np.array([220., 256., 304.])  # 260. + 20.
-        #self.angle_planet = (np.array([238, 234, 228]) - 25) * np.pi / 180.
-        self.a_planet = np.array([252.])
-        self.angle_planet = (np.array([231.]) - 25) * np.pi / 180.
+        #self.a_planet = np.array([252.])
+        #self.angle_planet = (np.array([231.]) - 25) * np.pi / 180.
+        self.a_planet = np.empty()
+        self.angle_planet = np.empty()
+        for i in range(10):
+            self.a_planet = np.append(self.a_planet, 252. + (2 * i - 2))
+            self.angle_planet = np.append(
+                self.angle_planet, (231 - 25) * np.pi / 180.)
 
         self.potential_planet = None
 
