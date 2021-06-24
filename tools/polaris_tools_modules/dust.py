e21bda137d6a86d54d8cc3477c0e1a0f4adf5a65
mlietzow <mlietzow@astrophysik.uni-kiel.de>
2021-06-24 16:47:29 +0200

    changed default .nk for silicate

--- tools/polaris_tools_modules/dust.py
+++ tools/polaris_tools_modules/dust.py
@@ -245,7 +245,7 @@ class Silicate(Dust):
         Dust.__init__(self, file_io, parse_args)
 
         # For creation of a dust catalog
-        self.parameter['dust_cat_file'] = 'silicate.nk'
+        self.parameter['dust_cat_file'] = 'silicate_d03.nk'
         self.parameter['scattering'] = 'MIE'
         self.parameter['material_density'] = 3500.
         # Minimum dust grain size
@@ -310,7 +310,7 @@ class Mrn(Dust):
         #: dict: Parameters which are different to the default values
         self.parameter['scattering'] = 'MIE'
         # Set the name of one component to allow print of sizes and wavelengths
-        self.parameter['dust_cat_file'] = 'silicate.nk'
+        self.parameter['dust_cat_file'] = 'silicate_d03.nk'
         # Minimum dust grain size
         self.parameter['amin'] = 5e-9
         # Maximum dust grain size
