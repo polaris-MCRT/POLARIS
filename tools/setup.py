cff311dd44841f04274bd936accc974d3d92d3f1
mlietzow <mlietzow@astrophysik.uni-kiel.de>
2022-04-01 10:14:13 +0200

    removed readme and long description

--- tools/setup.py
+++ tools/setup.py
@@ -37,8 +37,8 @@ for script in scripts:
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(os.path.join(here, 'README'), encoding='utf-8') as f:
-    long_description = f.read()
+# with open(os.path.join(here, 'README'), encoding='utf-8') as f:
+#     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -69,7 +69,7 @@ setup(
     #
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
-    long_description=long_description,  # Optional
+    # long_description=long_description,  # Optional
 
     # Denotes that our long_description is in Markdown; valid values are
     # text/plain, text/x-rst, and text/markdown
@@ -81,7 +81,7 @@ setup(
     #
     # This field corresponds to the "Description-Content-Type" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
+    # long_description_content_type='text/markdown',  # Optional (see note above)
 
     # This should be a valid link to your project's main homepage.
     #
