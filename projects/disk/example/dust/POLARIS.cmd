8f89a1b6f132033a78967dfe66efaf06196755ef
mlietzow <mlietzow@astrophysik.uni-kiel.de>
2022-04-20 13:45:10 +0200

    updated command files

--- projects/disk/example/dust/POLARIS.cmd
+++ projects/disk/example/dust/POLARIS.cmd
@@ -26,7 +26,7 @@
 <task> 1
 
 	# detector for dust emission = "N_pixel x N_pixel"> wavelength_min wavelength_max wavelength_nr ID_source rot_1 rot_2 distance
-	<detector_dust nr_pixel = "256*256">	0.00085	0.00085	1	1	0.0	0.0	4.319948614054068e+18
+	<detector_dust nr_pixel = "256*256">	0.00085	0.00085	1	1	30.0	0.0	4.319948614054068e+18
 
 	# background radiation source
 	# background = "N_photons"> scaling_factor temperature q u v rot_1 rot_2
@@ -40,7 +40,7 @@
 	<cmd>			CMD_DUST_EMISSION
 
 	# path of the input grid file
-	<path_grid>		"/YOUR/POLARIS/PATH/projects/disk/example/temp/grid.dat"
+	<path_grid>		"/YOUR/POLARIS/PATH/projects/disk/example/temp/grid_temp.dat"
 
 	# path for all output data
 	<path_out>		"/YOUR/POLARIS/PATH/projects/disk/example/dust/"
--- projects/disk/example/dust_mc/POLARIS.cmd
+++ projects/disk/example/dust_mc/POLARIS.cmd
@@ -3,9 +3,9 @@
 
 	# set the considered dust grain compositions
 	# "path_to_file" "size_keyword" mass_fraction mass_density radius_min radius_max exponent
-	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/silicate.nk" "plaw" 0.625 3500.0 5e-09 2.5e-07 -3.5
-	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/graphite_perpend.nk" "plaw" 0.25 2250 5e-09 2.5e-07 -3.5
-	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/graphite_parallel.nk" "plaw" 0.125 2250 5e-09 2.5e-07 -3.5
+	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/silicate_d03.nk" "plaw" 0.625 3500.0 5e-09 2.5e-07 -3.5
+	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/graphite_perpend_d03.nk" "plaw" 0.25 2250 5e-09 2.5e-07 -3.5
+	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/graphite_parallel_d03.nk" "plaw" 0.125 2250 5e-09 2.5e-07 -3.5
 
 	# phase function for dust scattering
 	<phase_function>	PH_MIE
@@ -26,7 +26,7 @@
 <task> 1
 
 	# detector for dust scattering = "N_pixel x N_pixel"> wavelength_min wavelength_max wavelength_nr rot_1 rot_2 distance
-	<detector_dust_mc nr_pixel = "256*256">	1e-06	1e-06	1	60.0	0.0	4.319948614054068e+18
+	<detector_dust_mc nr_pixel = "256*256">	1e-06	1e-06	1	30.0	0.0	4.319948614054068e+18
 
 	# star in the center as radiation source
 	# star = "N_photons"> pos_x pos_y pos_z radius temperature
@@ -40,7 +40,7 @@
 	<cmd>			CMD_DUST_SCATTERING
 
 	# path of the input grid file
-	<path_grid>		"/YOUR/POLARIS/PATH/projects/disk/example/temp/grid.dat"
+	<path_grid>		"/YOUR/POLARIS/PATH/projects/disk/grid.dat"
 
 	# path for all output data
 	<path_out>		"/YOUR/POLARIS/PATH/projects/disk/example/dust_mc/"
