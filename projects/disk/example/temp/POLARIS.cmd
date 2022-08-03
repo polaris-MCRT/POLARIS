ff0822d2066bf8918b07ee722de6fb8967a49999
mlietzow <mlietzow@astrophysik.uni-kiel.de>
2022-08-03 14:04:35 +0200

    updated exemplary command files, and quickstart guide

--- projects/disk/example/temp/POLARIS.cmd
+++ projects/disk/example/temp/POLARIS.cmd
@@ -3,9 +3,9 @@
 
 	# set the considered dust grain compositions
 	# "path_to_file" "size_keyword" mass_fraction mass_density radius_min radius_max exponent
-	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/silicate_d03.nk" "plaw" 0.625 3500.0 5e-09 2.5e-07 -3.5
-	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/graphite_perpend_d03.nk" "plaw" 0.25 2250 5e-09 2.5e-07 -3.5
-	<dust_component>	"/YOUR/POLARIS/PATH/input/dust/graphite_parallel_d03.nk" "plaw" 0.125 2250 5e-09 2.5e-07 -3.5
+	<dust_component>	"input/dust/silicate_d03.nk" "plaw" 0.625 3500.0 5e-09 2.5e-07 -3.5
+	<dust_component>	"input/dust/graphite_perpend_d03.nk" "plaw" 0.25 2250 5e-09 2.5e-07 -3.5
+	<dust_component>	"input/dust/graphite_parallel_d03.nk" "plaw" 0.125 2250 5e-09 2.5e-07 -3.5
 
 	# phase function for dust scattering
 	<phase_function>	PH_MIE
@@ -33,10 +33,10 @@
 	<cmd>			CMD_TEMP
 
 	# path of the input grid file
-	<path_grid>		"/YOUR/POLARIS/PATH/projects/disk/grid.dat"
+	<path_grid>		"projects/disk/grid.dat"
 
 	# path for all output data
-	<path_out>		"/YOUR/POLARIS/PATH/projects/disk/example/temp/"
+	<path_out>		"projects/disk/example/temp/"
 
 </task>
 
