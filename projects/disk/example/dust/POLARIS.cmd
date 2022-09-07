344448f2ff1e76471233a3d59705e2cb581d38b9
mlietzow <mlietzow@astrophysik.uni-kiel.de>
2022-09-07 17:41:44 +0200

    adjusted the default maximum level of subpixeling

--- projects/disk/example/dust/POLARIS.cmd
+++ projects/disk/example/dust/POLARIS.cmd
@@ -28,6 +28,9 @@
 	# detector for dust emission = "N_pixel x N_pixel"> wavelength_min wavelength_max wavelength_nr ID_source rot_1 rot_2 distance
 	<detector_dust nr_pixel = "256*256">	0.00085	0.00085	1	1	30.0	0.0	4.319948614054068e+18
 
+	# maximum level of subpixeling
+	<max_subpixel_lvl> 1
+
 	# background radiation source
 	# background = "N_photons"> scaling_factor temperature q u v rot_1 rot_2
 	<source_background nr_photons = "1e5">	0.0	0.0	0.0	0.0	0.0	0.0	0.0
