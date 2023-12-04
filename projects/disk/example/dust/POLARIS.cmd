5c3a7efc5fa94745c19d56911e696618569b8227
Moritz Lietzow-Sinjen <mlietzow@astrophysik.uni-kiel.de>
2023-12-04 10:15:22 +0000

    updated example dust command file

--- projects/disk/example/dust/POLARIS.cmd
+++ projects/disk/example/dust/POLARIS.cmd
@@ -28,10 +28,6 @@
 	# detector for dust emission = "N_pixel x N_pixel"> wavelength_min wavelength_max wavelength_nr ID_source rot_1 rot_2 distance
 	<detector_dust_polar nr_pixel = "256*256">	0.00085	0.00085	1	1	30.0	0.0	4.319948614054068e+18
 
-	# background radiation source
-	# background = "N_photons"> scaling_factor temperature q u v rot_1 rot_2
-	<source_background nr_photons = "1e5">	0.0	0.0	0.0	0.0	0.0	0.0	0.0
-
 	# rotation axis for first and second rotation angle
 	<axis1>	1	0	0
 	<axis2>	0	1	0
