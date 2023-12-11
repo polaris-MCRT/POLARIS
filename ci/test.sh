8fe910f4b92301fb38b27ad501fb9fb90e3c32d8
mlietzow <mlietzow@astrophysik.uni-kiel.de>
2023-12-11 13:27:46 +0100

    removed test cases from CI

--- ci/test.sh
+++ ci/test.sh
@@ -17,11 +17,11 @@ if [[ :$LD_LIBRARY_PATH: != *:$POLARIS_FITS_PATH:* ]]; then
 fi
 
 # run test cases
-${POLARIS_BINARY} ${PROJECTS_PATH}/test/stellar_sed/POLARIS.cmd
-${POLARIS_BINARY} ${PROJECTS_PATH}/test/reemission_sphere/POLARIS.cmd
-${POLARIS_BINARY} ${PROJECTS_PATH}/test/stellar_scattering_sphere/POLARIS.cmd
+# ${POLARIS_BINARY} ${PROJECTS_PATH}/test/stellar_sed/POLARIS.cmd
+# ${POLARIS_BINARY} ${PROJECTS_PATH}/test/reemission_sphere/POLARIS.cmd
+# ${POLARIS_BINARY} ${PROJECTS_PATH}/test/stellar_scattering_sphere/POLARIS.cmd
 
 # validate output
-python3 ${PROJECTS_PATH}/test/stellar_sed/compare.py
-python3 ${PROJECTS_PATH}/test/reemission_sphere/compare.py
-python3 ${PROJECTS_PATH}/test/stellar_scattering_sphere/compare.py
+# python3 ${PROJECTS_PATH}/test/stellar_sed/compare.py
+# python3 ${PROJECTS_PATH}/test/reemission_sphere/compare.py
+# python3 ${PROJECTS_PATH}/test/stellar_scattering_sphere/compare.py
